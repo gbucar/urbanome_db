2ad0528c77925acabdc6691f75fc8f63f165c5d7
user <59aur0ir@duck.com>
2025-02-23 19:22:37 +0100

    d

--- queries.sql
+++ queries.sql
@@ -1,20 +1,4 @@
-create table agg.noisecapture_filtered_record as
-select
-    *
-from
-    agg.noiscapture_nocar_record nncr
-where
-    (
-        nncr.record_uuid in (
-            select
-                nnsr.record_uuid
-            from
-                agg.noisecapture_nostatic_record nnsr
-        )
-        and nncr.duration_seconds > 20
-    );
-
-create table noisecapture_nocar_record_view as
+create table agg.noisecapture_nocar_record_view as
 SELECT
     *
 FROM
@@ -43,4 +27,61 @@ WHERE
             noisecapture_raw_point
         WHERE
             speed > 2.6
+    );
+
+create table agg.noisecapture_points_with_next_point as
+SELECT
+    record_uuid,
+    location_utc,
+    geom,
+    LEAD (geom) OVER (
+        PARTITION BY
+            record_uuid
+        ORDER BY
+            location_utc
+    ) AS next_geom
+FROM
+    noisecapture_raw_point nrp
+ORDER BY
+    location_utc;
+
+create table agg.noisecapture_nojump_record as
+select
+    *
+from
+    noisecapture_raw_record nrr
+where
+    nrr.record_uuid not in (
+        select distinct
+            record_uuid
+        from
+            agg.noisecapture_points_with_next_point nnp
+        where
+            ST_Distance (
+                ST_Transform (geom, 3857),
+                ST_Transform (next_geom, 3857)
+            ) > 70
+    );
+
+create table agg.noisecapture_filtered_record as
+select
+    *
+from
+    agg.noisecapture_nocar_record nncr
+where
+    (
+        nncr.record_uuid in (
+            select
+                nnsr.record_uuid
+            from
+                agg.noisecapture_nostatic_record nnsr
+        )
+        and nncr.record_uuid in (
+            select
+                nnsr.record_uuid
+            from
+                agg.noisecapture_nostatic_record nnsr
+        )
+        and nncr.duration_seconds > 20
+        and nncr.start_utc > '2024-01-01'
     );
